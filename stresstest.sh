b061b9064f4607e7253357f00885457d6c0ab96c
Johannes Unglert <unglert@b1-systems.de>
2019-05-14 12:54:40 +0200

    Fix 1: stress-command is now executed

--- stresstest.sh
+++ stresstest.sh
@@ -3,7 +3,7 @@ read -p "Runtime in seconds: " duration #in seconds
 read -p "Filename.csv: " save_dir #filename as a csv
 
 #echo "${save_dir}"
-#stress --cpu 4 --timeout ${duration} --quiet &
+stress --cpu 4 --timeout ${duration} --quiet &
 echo "Time; Temperature in °C; Frequency in MHz" | tee -a ${save_dir}
 
 for (( i=1; i<=${duration}; i++ ))
