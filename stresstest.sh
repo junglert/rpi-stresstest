461d58e11870df63153ba7cd4940221ccf4a2282
Johannes Unglert <47390613+junglert@users.noreply.github.com>
2019-05-15 08:29:35 +0200

    Fix 2: Removed some unused lines

--- stresstest.sh
+++ stresstest.sh
@@ -2,14 +2,12 @@
 read -p "Runtime in seconds: " duration #in seconds
 read -p "Filename.csv: " save_dir #filename as a csv
 
-#echo "${save_dir}"
 stress --cpu 4 --timeout ${duration} --quiet &
 echo "Time; Temperature in °C; Frequency in MHz" | tee -a ${save_dir}
 
 for (( i=1; i<=${duration}; i++ ))
 do
-        temptest=$(cat /sys/class/thermal/thermal_zone*/temp)
-        #temptest=$((temptest/10))
+        temptest=$(cat /sys/class/thermal/thermal_zone*/temp)    
         time=$(date +%T)
         frequency=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
         frequency=$((frequency/1000))
