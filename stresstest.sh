a85fffd2ea9a245b5f3b040816b8538e966c3e55
Johannes Unglert <47390613+junglert@users.noreply.github.com>
2020-01-31 10:26:55 +0100

    Update stresstest.sh

--- stresstest.sh
+++ stresstest.sh
@@ -5,9 +5,33 @@
 #license: GPL-3.0
 #summary: this bash script utilizes all cores of the RaspberryPi
 
+stresstest(){ #function for stressing the pi
+
+    echo "Checking if stress is installed, if not it´s going to be installed"
+    downloadstress
+
+    read -p "Runtime in seconds: " duration #in seconds
+    read -p "Filename.csv: " save_dir #filename as a csv
+
+    stress --cpu 4 --timeout ${duration} --quiet &
+    echo "Time       Temperature in °C   Frequency in MHz  on $(date)" | tee -a ${save_dir}
+
+        for (( i=1; i<=${duration}; i++ ))
+        do
+            temptest=$(cat /sys/class/thermal/thermal_zone*/temp)
+            time=$(date +%T)
+            frequency=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
+            frequency=$((frequency/1000))
+            echo "${time}   ${temptest}               ${frequency}" | tee -a ${save_dir}
+            sleep 1 
+        done
+}
+
+
 checkifdownloaded(){
 	dpkg -l stress &
 }
+
 
 downloadstress(){
 	if [ checkifdownloaded >/dev/null ]
@@ -42,27 +66,6 @@ Usage: 1. sudo ./stresstest.sh -s or --start  | start the script
     '
 }
 
-stresstest(){ #function for stressing the pi
-
-    echo "Checking if stress is installed, if not it´s going to be installed"
-    downloadstress
-
-    read -p "Runtime in seconds: " duration #in seconds
-    read -p "Filename.csv: " save_dir #filename as a csv
-
-    stress --cpu 4 --timeout ${duration} --quiet &
-    echo "Time       Temperature in °C   Frequency in MHz  on $(date)" | tee -a ${save_dir}
-
-        for (( i=1; i<=${duration}; i++ ))
-        do
-            temptest=$(cat /sys/class/thermal/thermal_zone*/temp)
-            time=$(date +%T)
-            frequency=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
-            frequency=$((frequency/1000))
-            echo "${time}   ${temptest}               ${frequency}" | tee -a ${save_dir}
-            sleep 1 
-        done
-}
 
 if [ "$1" == "-s" ] || [ "$1" == "--start" ];then #checking if input is -s or --start --> starting script
 stresstest
